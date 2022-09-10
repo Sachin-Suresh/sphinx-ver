2bd4982eb809880f280227268b1865290eda684e
Sachin-Suresh <Sachin301190@gmail.com>
2022-09-10 23:05:31 +0900

    simple pdf

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinx_simplepdf'
 ]
 
 intersphinx_mapping = {
@@ -34,100 +35,13 @@ html_theme = 'sphinx_rtd_theme'
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
 
-# -- Extension configuration -------------------------------------------------
+simplepdf_vars = {
+    'primary': '#FA2323',
+    'secondary': '#379683',
+    'cover': '#ffffff',
+    'white': '#ffffff',
+    'links': 'FA2323',
+    'cover-bg' 'url(cover-bg.jpg) no-repeat center'
+    'cover-overlay': 'rgba(250, 35, 35, 0.5)',
+}
 
-# add sourcecode to path
-import sys, os
-import logging
-logging.basicConfig(level=logging.INFO)
-sys.path.insert(0, os.path.abspath('../src'))
-
-############################
-# SETUP THE RTD LOWER-LEFT #
-############################
-try:
-   html_context
-except NameError:
-   html_context = dict()
-html_context['display_lower_left'] = True
-
-templates_path = ['_templates']
-
-if 'REPO_NAME' in os.environ:
-   REPO_NAME = os.environ['REPO_NAME']
-else:
-   REPO_NAME = ''
- 
-# SET CURRENT_LANGUAGE
-if 'current_language' in os.environ:
-   # get the current_language env var set by buildDocs.sh
-   current_language = os.environ['current_language']
-else:
-   # the user is probably doing `make html`
-   # set this build's current language to english
-   current_language = 'en'
- 
-# tell the theme which language to we're currently building
-html_context['current_language'] = current_language
-
-# SET CURRENT_VERSION
-from git import Repo
-from pprint import pprint
-repo = Repo( search_parent_directories=True )
-logging.info('----------');
-logging.info(repo);
-if 'current_version' in os.environ:
-   # get the current_version env var set by buildDocs.sh
-   current_version = os.environ['current_version']
-   logging.info('--if---');
-   logging.info(current_version);
-else:
-   # the user is probably doing `make html`
-   # set this build's current version by looking at the branch
-   #current_version = repo.active_branch.name   ISSUE HERE
-   current_version = repo.head.object.hexsha
-   logging.info('--else---');
-   logging.info(current_version);
- 
-# tell the theme which version we're currently on ('current_version' affects
-# the lower-left rtd menu and 'version' affects the logo-area version)
-html_context['current_version'] = current_version
-html_context['version'] = current_version
- 
-# POPULATE LINKS TO OTHER LANGUAGES
-html_context['languages'] = [ ('en', '/' +REPO_NAME+ '/en/' +current_version+ '/') ]
-logging.info('--REPO NAmE:---');
-logging.info(REPO_NAME);
- 
-languages = [lang.name for lang in os.scandir('locales') if lang.is_dir()]
-for lang in languages:
-   html_context['languages'].append( (lang, '/' +REPO_NAME+ '/' +lang+ '/' +current_version+ '/') )
-logging.info('--Languages:---');
-logging.info(os.scandir('locales'));
-
-# POPULATE LINKS TO OTHER VERSIONS
-html_context['versions'] = list()
- 
-versions = [branch.name for branch in repo.branches]
-for version in versions:
-   html_context['versions'].append( (version, '/' +REPO_NAME+ '/'  +current_language+ '/' +version+ '/') )
- 
-# POPULATE LINKS TO OTHER FORMATS/DOWNLOADS
- 
-# settings for creating PDF with rinoh
-#rinoh_documents = [(
-# master_doc,
-# 'target',
-# project+ ' Documentation',
-# '© ' +copyright,
-#)]
-#today_fmt = "%B %d, %Y"
- 
-# settings for EPUB
-#epub_basename = 'target'
- 
-html_context['downloads'] = list()
-html_context['downloads'].append( ('pdf', '/' +REPO_NAME+ '/' +current_language+ '/' +current_version+ '/' +project+ '-docs_' +current_language+ '_' +current_version+ '.pdf') )
- 
-html_context['downloads'].append( ('epub', '/' +REPO_NAME+ '/' +current_language+ '/' +current_version+ '/' +project+ '-docs_' +current_language+ '_' +current_version+ '.epub') )
- 
